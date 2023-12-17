87def4d08498a149d21929384a8e3706396f832e
M-A-dibshik <dibshik55@gamil.com>
2023-12-17 21:16:21 +0200

    header file

--- monty.h
+++ monty.h
@@ -39,6 +39,8 @@ typedef struct instruction_s
 } instruction_t;
 
 
-void push(stack_t **, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
 
 #endif
